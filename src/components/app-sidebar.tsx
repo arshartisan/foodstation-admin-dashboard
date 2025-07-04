a3eee2df534e62084e031e80850064b6efd0b7bc
focaldive <focaldive@gmail.com>
2025-07-04 11:12:03 +0530

    feat: replace dashboard icon with Bike and update Sidebar variant for improved UI

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import {
   LayoutDashboard,
+  Bike,
   Users,
   Store,
   Truck,
@@ -41,14 +42,14 @@ export function AppSidebar() {
   const location = useLocation();
 
   return (
-    <Sidebar collapsible="icon">
+    <Sidebar collapsible="icon" variant="floating">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link to="/dashboard">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-secondary text-sidebar-primary-foreground">
-                  <LayoutDashboard className="size-4 bg-[r" />
+                  <Bike className="size-4 bg-[r" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">FS Admin</span>
