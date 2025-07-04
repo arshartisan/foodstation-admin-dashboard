0e734d3df843e67273d32d1456fc76f5bf28acad
focaldive <focaldive@gmail.com>
2025-07-04 11:56:59 +0530

    refactor: remove useDocumentTitle hook from AuthLayout component

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,9 +1,8 @@
-import { Outlet } from "react-router-dom";
 import { useDocumentTitle } from "@/hooks";
+import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   useDocumentTitle();
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
